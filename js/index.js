ad1b4901d4b3c128db524e490b2f1a1ee164e349
Finn Llewellyn <llewellynfinn@gmail.com>
2021-02-14 21:10:00 +0000

    Fixed img and viewer

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", () => {
-    const viewer = HLViewer.init("#hlv-target", "../res");
+    const viewer = HLViewer.init("#hlv-target", "./res");
     viewer.load("kz_ea_highblock.dem");
     // viewer.setTitle("c3a2e.bsp");
 });
